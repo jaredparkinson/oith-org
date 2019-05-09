981b6c549fd039ecdcf096eb841db365fe98950b
Jared Parkinson <jared@parkinson.im>
2019-05-09 06:06:03 -0600

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChapterComponent } from './components/chapter/chapter.component';
+import { VerseComponent } from './components/verse/verse.component';
+import { ParagraphComponent } from './components/paragraph/paragraph.component';
+import { WComponent } from './components/w/w.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyBlockComponent } from './components/body-block/body-block.component';
+import { NoteComponent } from './components/note/note.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChapterComponent,
+    VerseComponent,
+    ParagraphComponent,
+    WComponent,
+    HeaderComponent,
+    SettingsComponent,
+    FooterComponent,
+    BodyBlockComponent,
+    NoteComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/verse/verse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verse',
+  templateUrl: './verse.component.html',
+  styleUrls: ['./verse.component.scss']
+})
+export class VerseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
