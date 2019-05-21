ea2e740c974dc6ae9a66123370a11c288dea3241
Jared Parkinson <jared@parkinson.im>
2019-05-21 12:10:59 -0600

    Adding sync scrolling

--- src/app/components/chapter/chapter.component.ts
+++ src/app/components/chapter/chapter.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { ParamService } from 'src/app/services/param.service';
 import { ChapterService } from 'src/app/services/chapter.service';
 import { SaveStateService } from 'src/app/services/save-state.service';
+import { element } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-chapter',
@@ -49,5 +50,78 @@ export class ChapterComponent implements OnInit {
         }
       },
     );
+  }
+
+  private scrollNotesTop(): void {
+    const note = document.querySelector('note');
+    if (note) {
+      note.scrollIntoView({ behavior: 'auto' });
+    }
+  }
+  public scrollNoteIntoView(belowTop: Element[]): void {
+    // let note: Element | undefined;
+    if (
+      ['title_number1', 'study_summary1', 'title1'].includes(belowTop[0].id)
+    ) {
+      this.scrollNotesTop();
+    } else {
+      const note = document.querySelector(
+        `#${belowTop[0].id.replace('p', 'note')}`,
+      );
+      if (note) {
+        setTimeout((): void => {
+          note.scrollIntoView({ behavior: 'auto' });
+        }, 300);
+      }
+    }
+  }
+
+  private scrollNotesBottom(): void {
+    const notes = document.querySelectorAll('note');
+    if (notes && notes[notes.length - 1]) {
+      notes[notes.length - 1].scrollIntoView({ behavior: 'auto' });
+    }
+  }
+
+  private scrollIntoView(belowTop: Element[]): void {
+    if (belowTop.length > 0) {
+      this.scrollNoteIntoView(belowTop);
+    } else {
+      this.scrollNotesBottom();
+    }
+  }
+  public onScroll(): void {
+    setTimeout((): void => {
+      const verseElements = document.querySelectorAll('verse');
+      // console.log(verseElements.length);
+      if (verseElements && verseElements.length > 0) {
+        if (verseElements[0].getBoundingClientRect().top > 34) {
+          this.scrollNotesTop();
+        } else {
+          const belowTop: Element[] = Array.from(verseElements)
+            .map(
+              (verse): Element | undefined => {
+                if (verse.getBoundingClientRect().top > 34) {
+                  // console.log(verse);
+
+                  return verse;
+                } else {
+                  return undefined;
+                }
+              },
+            )
+            .filter(
+              (element): boolean => {
+                return element !== undefined;
+              },
+            ) as Element[];
+          if (verseElements) {
+          }
+          // console.log(belowTop.length);
+
+          this.scrollIntoView(belowTop);
+        }
+      }
+    }, 100);
   }
 }
